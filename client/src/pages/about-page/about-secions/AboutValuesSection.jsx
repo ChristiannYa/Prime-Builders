ddd15ce33ca845264493bba5243efd566e1230ee
ChristiannYa <christian.ylopez02@gmail.com>
2025-03-22 15:51:12 -0500

    page divisor fix w-screen

--- client/src/pages/about-page/about-secions/AboutValuesSection.jsx
+++ client/src/pages/about-page/about-secions/AboutValuesSection.jsx
@@ -4,14 +4,23 @@ import aboutAssets from '../../../assets/about-page';
 const AboutValuesSection = () => {
   return (
     <div className="bg-neutral-300 pb-18 max-md:pb-12 pt-28 max-md:pt-18 relative">
-      <img src={aboutAssets.layeredCurves} alt="" className='w-full absolute-top-left'/>
+      <img
+        src={aboutAssets.layeredCurves}
+        alt=""
+        className="w-screen absolute-top-left"
+      />
       <div className="screen-xl">
-      <h3 className="textTitle text-secondary text-center font-medium mb-7">Why Choose Us?</h3>
-        <div className='aboutValues'>
+        <h3 className="textTitle text-secondary text-center font-medium mb-7">
+          Why Choose Us?
+        </h3>
+        <div className="aboutValues">
           {companyValues.map((value, index) => (
-            <div key={index} className="bg-neutral-200 rounded-sm shadow-card p-3">
+            <div
+              key={index}
+              className="bg-neutral-200 rounded-sm shadow-card p-3"
+            >
               <h3 className="textSubtitle text-secondary">{value.title}</h3>
-              <p className='textCard text-secondary'>{value.description}</p>
+              <p className="textCard text-secondary">{value.description}</p>
             </div>
           ))}
         </div>
