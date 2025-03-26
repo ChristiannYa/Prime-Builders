88c2cd3658e20afd4be786212d57a13ec8ea2cc5
ChristiannYa <christian.ylopez02@gmail.com>
2025-03-25 23:29:53 -0500

    fixed url

--- server/config/email/environment.mjs
+++ server/config/email/environment.mjs
@@ -1,7 +1,7 @@
 export const config = {
   port: process.env.PORT || 3000,
   cors: {
-    origin: ['https://primebuildercpt.netlify.app', 'http://localhost:5173'],
+    origin: ['https://primebuildersctp.netlify.app/', 'http://localhost:5173'],
     methods: ['POST', 'GET'],
     credentials: true,
   },
